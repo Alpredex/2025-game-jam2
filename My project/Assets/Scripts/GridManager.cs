9baee25ade474b438bc374830438849f21348555
Alpredex <118100450+Alpredex@users.noreply.github.com>
2025-02-16 12:24:03 -0500

    BOUTTA PLAY WITH BABY

--- My project/Assets/Scripts/GridManager.cs
+++ My project/Assets/Scripts/GridManager.cs
@@ -13,12 +13,42 @@ public class GridManager : MonoBehaviour
 
     private void Awake()
     {
+        CreateGrid();
+    }
+
+    public Node GetNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates)) {
+            return grid[coordinates];
+        }
+        return null;
+    }
+
+    public void BlackNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
+        { 
+            grid[coordinates].walkable = false;
+        }
+    }
+
+    public void ResetNode() {
+        foreach (KeyValuePair<Vector2Int, Node> entry in grid)
+        {
+            entry.Value.connectTo = null;
+            entry.Value.explored = false;
+            entry.Value.path = false;
+        }
+    }
+
+    private void CreateGrid()
+    {
         for (int x = 0; x < gridSize.x; x++)
         {
             for (int y = 0; y < gridSize.y; y++)
             {
                 Vector2Int cords = new Vector2Int(x, y);
-                grid.Add(cords, new Node(cords));
+                grid.Add(cords, new Node(cords, true));
 
                 //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 //Vector3 position = new Vector3(cords.x * unitGridSize, 0f, cords.y * unitGridSize);
